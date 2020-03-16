7f7627d48f8593295516922f4a92800e6ada8a3c
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-03-16 11:46:22 +0300

    Finish Add Machine item to Cart and add Tool item to Cart after click ADDTOCART Buttem

--- project3-frontend/src/machine/components/machine.js
+++ project3-frontend/src/machine/components/machine.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import {addToCart} from '../../cart/cartAPI';
 
 
 export default class Machine extends React.Component{
   
+  addToCartMachine = (event)=>{
+    event.preventDefault();
+
+    let cartItem ={
+      itemId : this.props.id ,
+      totalprice:55 ,
+       name: this.props.name,
+      price: this.props.price, 
+      amount : 9 ,
+     img: this.props.img
+     } 
+    
+
+    addToCart(cartItem) 
+    .then ((response)=>{
+        console.log("Item has been Added");
+        const  newCartList = this.props.carts;
+        newCartList.push(cartItem);
+        console.log(newCartList)
+        this.props.setCarts(newCartList)
+
+    })
+    .catch((error) =>{
+        console.log('API error' , error)
+    })
+  }
 
   render(){
 
@@ -13,7 +40,7 @@ export default class Machine extends React.Component{
        <p> {this.props.type} </p>
        <p> {this.props.price} </p>
        <p> {this.props.img} </p>
-       <a href ="#">ADD TO CART</a>
+       <a href ="#" onClick={this.addToCartMachine}>ADD TO CART</a>
 </div>
   )
 }
