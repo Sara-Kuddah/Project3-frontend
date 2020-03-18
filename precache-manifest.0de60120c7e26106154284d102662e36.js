55f935e609a6e0e4efc99a190456f9fcaaa66003
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-03-18 16:32:43 +0300

    Updates

---
+++ precache-manifest.0de60120c7e26106154284d102662e36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ba919c2ce712fe22f95b7a7025c486",
+    "url": "/Project3-frontend/index.html"
+  },
+  {
+    "revision": "c93c44a9ef76294f129e",
+    "url": "/Project3-frontend/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0fbd00648b7e92dcc779",
+    "url": "/Project3-frontend/static/css/main.6f2d43af.chunk.css"
+  },
+  {
+    "revision": "c93c44a9ef76294f129e",
+    "url": "/Project3-frontend/static/js/2.aee9e6e2.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Project3-frontend/static/js/2.aee9e6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fbd00648b7e92dcc779",
+    "url": "/Project3-frontend/static/js/main.894a2d1d.chunk.js"
+  },
+  {
+    "revision": "5d51aca308a6385c9983",
+    "url": "/Project3-frontend/static/js/runtime-main.df026ae1.js"
+  }
+]);
